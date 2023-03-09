c1c656e7f1f510946d535b7609e2ef2826342293
AndrewGalatsan <agalatsan@hotmail.com>
2023-03-09 01:47:14 +0000

    updated

--- helpers/notify.js
+++ helpers/notify.js
@@ -3,59 +3,56 @@
 
 require('dotenv').config();
 const ENV        = process.env.ENV || "development";
-// const express    = require("express");
-// const app        = express();
+const express    = require("express");
+const app        = express();
 
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-// const twilioSMSAPI = () => {
+const twilioSMSAPI = (options) => {
   const accountSid = process.env.TWILIO_SID;
   const authToken = process.env.TWILIO_AUTH_TOKEN;
   const client = require("twilio")(accountSid, authToken);
-
+  console.log('test')
   // return function (options) {
     client.messages
       .create({
-        body: 'step 1',
-        from: "+16477782876",
+        body: `${options.text}`,
+        from: "+15675871752",
         to: "+16477782876",
       })
       .then((message) => console.log(message.sid))
-      // .catch((error) => console.log(error));
-  // };
-// };
-// const notifySMS = twilioSMSAPI();
-
-// const notifyOwner = (id = 1234, phoneNumber = "step 2") => {
-//   const options = {
-//     text: `New Order is placed`,
-//     phoneNumber: "+16477782876",
-//   };
-//   // notifySMS(options);
+      .catch((error) => console.log(error));
+  };
 // };
 
-// const notifyCustomerOrderConfirmed = (
-//   id = 7,
-//   waitTime = 15,
-//   phoneNumber = "+16477782876"
-// ) => {
-//   const options = {
-//     text: `step 3!`,
-//     phoneNumber: "+16477782876",
-//   };
-//   // notifySMS(options);
-// };
+const notifyOwner = () => {
+  const options = {
+    text: `New Order is placed`
+  };
+  twilioSMSAPI(options);
+};
 
-// const notifyCustomerOrderReady = (id = 1234, phoneNumber = "+16048805245") => {
-//   const options = {
-//     text: `Your order is ready for a pickup.`,
-//     phoneNumber,
-//   };
-//   // notifySMS(options);
-// };
+const notifyCustomerOrderConfirmed = (
+  id = 7,
+  waitTime = 15,
+  phoneNumber = "+16477782876"
+) => {
+  const options = {
+    text: `Your order will be ready in ${waitTime}!`,
+    phoneNumber: "+16477782876",
+  };
+  twilioSMSAPI(options);
+};
 
-// module.exports = {
-//   notifyOwner,
-//   notifyCustomerOrderConfirmed,
-//   notifyCustomerOrderReady,
-// };
+const notifyCustomerOrderReady = () => {
+  const options = {
+    text: `Your order is ready for a pickup.`,
+  };
+  twilioSMSAPI(options);
+};
+
+module.exports = {
+  notifyOwner,
+  notifyCustomerOrderConfirmed,
+  notifyCustomerOrderReady,
+};
--- routes/confirmation.js
+++ routes/confirmation.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const db = require('../db/connection');
 const router  = express.Router();
-
+const {notifyOwner, notifyCustomerOrderConfirmed,notifyCustomerOrderReady} = require('../helpers/notify')
 
 
 
@@ -14,13 +14,6 @@ module.exports = (db) => {
   let totalQuantity = 0;
   let waitTime = 0;
 
-
-
-
-  const accountSid = 'ACdf5a7fd0dd98ae7a672b705a9bcb9591';
-  const authToken = '8aae7aa726fe12ad008b759dabd13731';
-  const twilio = require('twilio');
-  const client = new twilio(accountSid, authToken);
 
 
   router.post("/", (req, res) => {      // JSON-only route for AJAX POST
@@ -40,21 +33,22 @@ module.exports = (db) => {
     customerInfo.name = selectedMenuItem.customerName
     customerInfo.phone = selectedMenuItem.customerPhone
     customerInfo.notes = selectedMenuItem.customerNotes
-console.log(req.body)
+
 
     db.query(`INSERT INTO users (name, phone) VALUES ($1, $2) returning *`, [customerInfo.name, customerInfo.phone])
               .then ( (data) => {
-                console.log(data)
-                db.query(`INSERT INTO food_orders (user_id, customer_notes, status)
-              VALUES ($1, $2, $3)`, [data.rows[0].id, customerInfo.notes, false ])})
+                return db.query(`INSERT INTO food_orders (user_id, customer_notes, status)
+              VALUES($1, $2, $3) returning *`, [data.rows[0].id, customerInfo.notes, false ])})
+              .then((foodOrder) => {
+                for (let item in confirmedItems) {
+                  db.query(`INSERT INTO ordered_items (order_id, menu_item_id, qty)
+                  VALUES ($1, $2, $3)`, [foodOrder.rows[0].id, confirmedItems[item].id, confirmedItems[item].qty]);
+                };
+                res.send('hello')
+              })
+
              // VALUES ('$1,$2,$3,$4,$5,$6'),[data.user_id, data.created_at, data.updated_at, data.completed_at, data.customer_notes, data.status];`)})
 
-
-    for (let item in confirmedItems) {
-      db.query(`INSERT INTO ordered_items (order_id, menu_item_id, qty)
-      VALUES (orders.id, 2, '${confirmedItems[item].id}', ${confirmedItems[item].qty});`)
-    };
-    res.send('hello')
   });
 
 
@@ -66,14 +60,9 @@ console.log(req.body)
     } else {
       waitTime = 15;
     }
-    client.messages
-    .create({
-      body: `Your order will be ready in ${waitTime} minutes.`,
-      from: '16472659126',
-      to: `16473680503`
-    }).then((message) => {
-      console.log(message.sid)
-    })
+    notifyOwner()
+    notifyCustomerOrderConfirmed(waitTime)
+    setTimeout(notifyCustomerOrderReady, waitTime * 1000)
 
     res.render("confirmation", { confirmedItems, prices, customerInfo });
     });
